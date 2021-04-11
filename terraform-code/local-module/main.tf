77988ac1d4299ab033e791dae729565d2c0892d5
root <root@ip-172-31-39-221.us-east-2.compute.internal>
2021-04-11 10:23:46 +0000

    adding a module

---
+++ terraform-code/local-module/main.tf
@@ -0,0 +1,22 @@
+# Demostration of pass agruments in module using variable
+module "terraform-module" {
+  source = "github.com/saravananajay/terraform-modules-ec2.git"
+    region                     = "${var.region}"
+    ami_id                     = "${var.ami_id}"
+    instance_type              = "${var.instance_type}"
+    tag                        = "${var.tag}"
+
+}
+
+# # Demostration of pass agruments in module
+# module "module-example" {
+#   source = "github.com/Patelvijaykumar/terraform-aws-instance-template.git"
+#
+#     region                     = "us-east-1"
+#     ami_id                     = "ami-035b3c7efe6d061d5"
+#     instance_type              = "t2.micro"
+#     tag                        = "module example"
+#
+# }
+
+
