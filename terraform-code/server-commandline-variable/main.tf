7d8b5ea17aaa2dfb9c530052364e567059b36d60
saravananajay <34689245+saravananajay@users.noreply.github.com>
2022-10-23 12:38:19 +0530

    updating ami

--- terraform-code/server-commandline-variable/main.tf
+++ terraform-code/server-commandline-variable/main.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "web-server" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "var.ami"
   instance_type = var.hardware
   key_name      = var.keyname
-  vpc_security_group_ids = ["sg-04e22ffb653c9b648"]
   tags = {
     Name = "This server by terraform code"
   }
