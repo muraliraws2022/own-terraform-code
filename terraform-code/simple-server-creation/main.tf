baed7c1ee8898aea24ac54ee847b00c3edb43cf7
saravananajay <34689245+saravananajay@users.noreply.github.com>
2022-10-23 12:19:19 +0530

    Update main.tf

--- terraform-code/simple-server-creation/main.tf
+++ terraform-code/simple-server-creation/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-server" {
   ami           = "ami-0d6ba217f554f6137"
-  instance_type = "t3.micro"
-  key_name      = "demo"
+  instance_type = "t2.micro"
+  key_name      = "night"
   tags = {
     Name = "This server by manual"
   }
