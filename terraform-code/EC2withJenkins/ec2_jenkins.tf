6177957605c0446d2c05c90700e2f73c65804e0c
muraliraws2022 <105298757+muraliraws2022@users.noreply.github.com>
2023-03-18 11:33:42 +0530

    Update ec2_jenkins.tf

--- terraform-code/EC2withJenkins/ec2_jenkins.tf
+++ terraform-code/EC2withJenkins/ec2_jenkins.tf
@@ -16,16 +16,14 @@ resource "aws_instance" "ec2_jenkins" {
 		sudo service httpd enable
 		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 
-    yum install java-1.8.0-openjdk-devel -y
-    curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
-    sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-    sudo sed -i 's/gpgcheck=1/gpgcheck=0/' /etc/yum.repos.d/jenkins.repo	
-    yum install -y jenkins
-    systemctl start jenkins
-    systemctl status jenkins
-    systemctl enable jenkins
+   sudo amazon-linux-extras install java-openjdk11 -y
+   sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+   sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+   sudo yum install jenkins -y
+   sudo systemctl start jenkins
+   sudo systemctl enable jenkins
 
-	   EOF
+	EOF
 
   tags = {
     Name = "Ec2-User-data"
