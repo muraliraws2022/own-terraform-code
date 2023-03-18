bfd488797ff9d16be7655beb7b01c003928b5328
muraliraws2022 <105298757+muraliraws2022@users.noreply.github.com>
2023-03-18 11:31:19 +0530

    Update ec2_jenkins.tf

--- terraform-code/EC2withJenkins/ec2_jenkins.tf
+++ terraform-code/EC2withJenkins/ec2_jenkins.tf
@@ -9,21 +9,19 @@ resource "aws_instance" "ec2_jenkins" {
   key_name = "${var.key_name}"
 
   user_data = <<EOF
-		#! /bin/bash
+                #! /bin/bash
                 sudo yum update -y
-		sudo yum install -y httpd.x86_64
-		sudo service httpd start
-		sudo service httpd enable
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+                sudo yum install -y httpd.x86_64
+                sudo service httpd start
+                sudo service httpd enable
+                echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 
-    yum install java-1.8.0-openjdk-devel -y
-    curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
-    sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-    sudo sed -i 's/gpgcheck=1/gpgcheck=0/' /etc/yum.repos.d/jenkins.repo	
-    yum install -y jenkins
-    systemctl start jenkins
-    systemctl status jenkins
-    systemctl enable jenkins
+   sudo amazon-linux-extras install java-openjdk11 -y
+   sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+   sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+   sudo yum install jenkins -y
+   sudo systemctl start jenkins
+   sudo systemctl enable jenkins
 
 	   EOF
 
